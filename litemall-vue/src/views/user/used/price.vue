4ff3cfee8857e47c887c8b19a8b2055f0aad0486
cannyyh <5207450+cannyyh@user.noreply.gitee.com>
2020-07-07 17:50:43 +0800

    price

--- litemall-vue/src/views/user/used/price.vue
+++ litemall-vue/src/views/user/used/price.vue
@@ -1,12 +1,12 @@
 <template>
 <van-form @submit="onSubmit">
-<!-- 
-  <van-field
-    v-model="price"
-    name="价格"
-    placeholder="函数校验"
-    :rules="[{ validator, message: '请输入正确内容' }]"
-  /> -->
+<van-panel>
+   <img src="http://testavatar.xingqiuxiuchang.cn/e208b75e-5228-4095-89a9-647e80aa708a.jpg" class="product-img"/>
+   <b class="icon">确认宝贝价格发布</b>
+</van-panel>
+<van-cell-group>
+  <van-field v-model="price" label="价格" placeholder="" />
+</van-cell-group>
   <div style="margin: 16px;">
     <van-button round block type="info" native-type="submit">
       确认发布
@@ -18,6 +18,7 @@
 <script>
 import {
   Form,
+  Field,
 } from 'vant';
 export default {
   data() {
@@ -32,12 +33,34 @@ export default {
   },
   components: {
     [Form.name]: Form,
+    [Field.name]: Field,
   }
 };
 </script>
 
 
 <style lang="scss" scoped>
-
-
+.van-cell{
+  border-bottom:1px solid #333333;
+}
+.product-img{
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  margin: 50px auto 0;
+  display: block;
+}
+.icon{
+  width: 100%;
+  text-align: center;
+  font-size: 18px;
+  display: block;
+}
+.van-cell-group{
+  width: 320px;
+  margin: 20px auto;
+}
+.van-field{
+  background: #f2f2f2;
+}
 </style>
