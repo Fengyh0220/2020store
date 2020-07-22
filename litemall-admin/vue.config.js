0913e02068ebc3650ae024019dd8e75b8f1d4e3f
Fengyh0220 <1416847041@qq.com>
2020-07-23 02:02:23 +0800

    add.

--- litemall-admin/vue.config.js
+++ litemall-admin/vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = '888商城后台管理' // page title
+const name = '亚马逊亚洲商城后台管理' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
